869f5add5579309d747c626414c56b04511200a0
davidrainis <david@jubaan.com>
2025-03-27 09:15:37 +0000

    updated all params for the new DNS: oedc.gixam.com

--- psql.py
+++ psql.py
@@ -44,7 +44,7 @@ def select_data(conn, select_query):
 
 
 CREATE_TABLE_QUERY = """
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS test_users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -55,18 +55,18 @@ CREATE TABLE IF NOT EXISTS users (
 
 if __name__=="__main__":
     # Replace these with your database credentials
-    dbname = "gixam"
-    user = "jubaanedc"
-    password = "Gixam!23!23!23"
+    dbname = "postgres"
+    user = "postgres.your-tenant-id"
+    password = "R3vyPCrVN0sMYdklJ2Rp13Y5"
     host = "localhost"
     port = "5432"
 
     conn = create_connection(host, port, user, password, dbname)
-   # execute_query(conn, CREATE_TABLE_QUERY)
-    insert = "insert into users (name, email, age) values ('jim1', 'jim1@kuki.com', 26);"
+    execute_query(conn, CREATE_TABLE_QUERY)
+    insert = "insert into test_users (name, email, age) values ('jim132', 'jim132@kuki.com', 26);"
     execute_query(conn, insert)
 
-    rows = select_data(conn, "SELECT * FROM users;")
+    rows = select_data(conn, "SELECT * FROM test_users;")
     for r in rows:
         print(r)
     close_connection(conn)
--- welcome.py
+++ welcome.py
@@ -1,4 +1,12 @@
+
+"""
+testing that the python env: pconfig works
+"""
+
 import os
+import sys
 
 
+print("Virtual Environment:", os.path.basename(sys.prefix))
+print("Virtual Environment working dir:", os.environ.get('VIRTUAL_ENV', 'Not in a virtual environment'))
 print(f"working on: {os.name}")
